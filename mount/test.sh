63d5520073dbbe652358a5e5fc2ccfe5f62af529
JonM0 <61517667+JonM0@users.noreply.github.com>
2024-10-24 16:54:49 +0200

    Cleanup test and add pfifo without age testcase

--- mount/test.sh
+++ mount/test.sh
@@ -7,44 +7,33 @@ timestamp=$(date +%s)
 echo "Starting test at $timestamp"
 
 
-echo "Resetting the network"
+echo "Resetting the network to codel"
 nft delete table inet prioritize || true
 tc qdisc del root dev ens4 || true
 sleep 1
 
-
-# echo "Starting ss"
-# ss --oneline --events --info state all dst server > /mnt/shared/logs/ss-log-$timestamp-def.txt &
-# sleep 10
-
-
-echo "Starting iperf2 traffic"
-/mnt/shared/iperf-test.sh | grep  0.0000- > /mnt/shared/logs/iperf-log-$timestamp-def.txt
-# sleep 10
-# killall ss
+echo "Starting iperf2 test"
+/mnt/shared/iperf-test.sh | grep  0.0000- > /mnt/shared/logs/iperf-log-$timestamp-codel.txt
+echo "Test finished"
+sleep 2
 
 
+echo "Enabling pfifo_fast"
+tc qdisc replace root dev ens4 pfifo_fast
+sleep 1
+
+echo "Starting iperf2 test"
+/mnt/shared/iperf-test.sh | grep  0.0000- > /mnt/shared/logs/iperf-log-$timestamp-pfifo.txt
 echo "Test finished"
 sleep 2
 
 
 echo "Enabling age based prioritization"
 nft -f /mnt/shared/rulesets/prioritize.nft
-tc qdisc replace root dev ens4 pfifo_fast
 sleep 1
-
-
-# echo "Starting ss"
-# ss --oneline --events --info state all dst server > /mnt/shared/logs/ss-log-$timestamp-age.txt &
-# sleep 10
-
 
 echo "Starting iperf2 test"
 /mnt/shared/iperf-test.sh | grep  0.0000- > /mnt/shared/logs/iperf-log-$timestamp-age.txt
-# sleep 10
-# killall ss
-
-
 echo "Test finished"
 
 
