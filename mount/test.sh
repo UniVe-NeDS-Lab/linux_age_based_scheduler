aa4310aeb1803bfb1d9781bc7dad7f6b40ad0bdd
JonM0 <61517667+JonM0@users.noreply.github.com>
2024-11-05 13:13:11 +0000

    Use a tc qdisc setup that supports stats

--- mount/test.sh
+++ mount/test.sh
@@ -3,28 +3,38 @@ set -e
 trap "exit" INT TERM
 trap "kill 0" EXIT
 
+ssh="sshpass -p password ssh -oStrictHostKeyChecking=no debian@server"
+
 timestamp=$(date +%s)
 echo "Starting test at $timestamp"
 
-
 echo "Resetting the network to codel"
 nft delete table inet prioritize || true
+# $ssh sudo nft delete table inet prioritize || true
 tc qdisc del root dev ens4 || true
+# $ssh sudo tc qdisc del root dev ens4 || true
 sleep 1
 
 echo "Starting iperf2 test"
-/mnt/shared/iperf-test.sh | grep  0.0000- > /mnt/shared/logs/iperf-log-$timestamp-codel.txt
+/mnt/shared/iperf-test.py > /mnt/shared/logs/iperf-log-$timestamp-codel.txt
 echo "Test finished"
+tc -s qdisc show dev ens4
 sleep 2
 
 
 echo "Enabling pfifo_fast"
-tc qdisc replace root dev ens4 pfifo_fast
+
+tc qdisc add dev ens4 root handle 1: prio bands 3
+tc qdisc add dev ens4 parent 1:1 handle 10: codel
+tc qdisc add dev ens4 parent 1:2 handle 20: codel
+tc qdisc add dev ens4 parent 1:3 handle 30: codel
+
 sleep 1
 
 echo "Starting iperf2 test"
-/mnt/shared/iperf-test.sh | grep  0.0000- > /mnt/shared/logs/iperf-log-$timestamp-pfifo.txt
+/mnt/shared/iperf-test.py > /mnt/shared/logs/iperf-log-$timestamp-pfifo.txt
 echo "Test finished"
+tc -s qdisc show dev ens4
 sleep 2
 
 
@@ -33,8 +43,9 @@ nft -f /mnt/shared/rulesets/prioritize.nft
 sleep 1
 
 echo "Starting iperf2 test"
-/mnt/shared/iperf-test.sh | grep  0.0000- > /mnt/shared/logs/iperf-log-$timestamp-age.txt
+/mnt/shared/iperf-test.py > /mnt/shared/logs/iperf-log-$timestamp-age.txt
 echo "Test finished"
+tc -s qdisc show dev ens4
 
 
 echo "Resetting the network"
--- virtlab/docs/test.md
+++ virtlab/docs/test.md
@@ -11,18 +11,35 @@ iperf-test.sh
 
 To enable age based priority: 
 ```
-sudo nft -f /mnt/shared/nftables/priority.nft
+sudo nft -f /mnt/shared/rulesets/prioritize.nft
 sudo tc qdisc replace root dev ens4 pfifo_fast
 ```
 
 To reset default queueing:
 ```
-sudo nft delete table inet prioritizeflows
+sudo nft delete table inet prioritize
 sudo tc qdisc del root dev ens4
 ```
 
 To check:
 ```
 sudo nft list ruleset
-sudo tc qdisc
+sudo tc -s qdisc show dev ens4
+```
+
+```
+sudo tc qdisc add dev ens4 root handle 1: prio bands 3
+sudo tc qdisc add dev ens4 parent 1:1 handle 10: codel
+sudo tc qdisc add dev ens4 parent 1:2 handle 20: codel
+sudo tc qdisc add dev ens4 parent 1:3 handle 30: codel
+```
+
+
+```
+sudo nft -f /mnt/shared/rulesets/prioritize.nft
+sudo tc qdisc del root dev ens4
+sudo tc qdisc add dev ens4 root handle 1: prio bands 3
+sudo tc qdisc add dev ens4 parent 1:1 handle 10: codel
+sudo tc qdisc add dev ens4 parent 1:2 handle 20: codel
+sudo tc qdisc add dev ens4 parent 1:3 handle 30: codel
 ```
