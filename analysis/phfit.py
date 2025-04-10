23c591bac21343ace79b604c71981b572db4678f
JonM0 <61517667+JonM0@users.noreply.github.com>
2025-04-10 02:32:05 +0200

    Add library to call phfit

---
+++ analysis/phfit.py
@@ -0,0 +1,78 @@
+import pathlib
+import subprocess
+import tempfile
+
+import numpy as np
+
+_CONTFIT_PATH = pathlib.Path('../PhFit/V1/classes/contfit').absolute()
+
+
+def _contfit_run(input_file: str | pathlib.Path, body: int, tail: int, **kwargs) -> subprocess.CompletedProcess:
+    return subprocess.run(
+        [
+            str(_CONTFIT_PATH),
+            '3',  # Fitting a distribution given by samples
+            input_file,  # name of file describing the distribution
+            '1',  # type of file: 1: empirical cdf
+            str(body),  # Number of Phases to Fit the Main Part
+            '-0.01',  # Limit of Fitting the Main Part
+            '1',  # Distance Measure: "1": Entropy
+            str(tail),  # Number of Phases to Fit the Tail Part
+            '-0.1',  # Lower Limit of Tail fitting
+            '-0.001',  # Upper Limit of Tail Fitting
+            '0.4',  # Distance of Adjacent Points
+            '0',  # Special Tail: "0" No special tail
+            '0',  # Parameter of the tail
+            '0',  # Start of the tail (positive->itself, negative->quantile)
+            '1',  # Number of round
+            '200',  # Number of iterations in a round
+            '10',  # Number of intervals for integrataion
+        ],
+        check=True,
+        capture_output=True,
+        text=True,
+        **kwargs,
+    )
+
+
+def _parse_contfit_matrix_output(data: str):
+    a_line, data = data.split('\n', 1)
+    b_lines, _ = data.split('f=', 1)
+
+    a = a_line.strip().removeprefix('a=[').removesuffix('];')
+    a = [float(x) for x in a.split()]
+
+    b = b_lines.strip().removeprefix('B=[').removesuffix(';\n];')
+    b = [list(map(float, x.split())) for x in b.split(';') if x]
+
+    return np.array(a), np.array(b)
+
+
+def contfit(x, cdf, body: int, tail: int):
+    """
+    Fit a phase-type distribution to the given CDF.
+
+    Parameters:
+    x: Array of sample points
+    cdf: Array of corresponding CDF values
+    n: Number of phases for the main part
+    m: Number of phases for the tail part
+
+    Returns:
+    a: Array of initial probabilities for the phases
+    b: Sub-infinitesimal generator matrix
+    """
+
+    cdf_file_content = '\n'.join(f'{xi} {pi}' for xi, pi in zip(x, cdf) if 0 < pi < 1)
+
+    with tempfile.TemporaryDirectory(prefix='phfit.') as tmpdir:
+        with open(f'{tmpdir}/cdf.txt', 'w') as file:
+            file.write(cdf_file_content)
+
+        _contfit_run('cdf.txt', body=body, tail=tail, cwd=tmpdir)
+
+        with open(f'{tmpdir}/TEMPORARYPhFitFILE.matrix') as f:
+            matrix = f.read()
+
+    a, b = _parse_contfit_matrix_output(matrix)
+    return a, b
