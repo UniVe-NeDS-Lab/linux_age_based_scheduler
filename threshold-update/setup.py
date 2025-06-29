b366fc7104bf0140633e08c0e180615dc83f1748
JonM0 <61517667+JonM0@users.noreply.github.com>
2025-06-29 16:17:02 +0200

    Remove threshold update

--- threshold-update/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-
-# the c++ extension module
-extension_mod = Extension("threshold_update", ["threshold_update.c"])
-
-setup(name = "threshold_update", ext_modules=[extension_mod])
-
